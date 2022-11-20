96b9db8aa5b6a9c8e148ede596fb187c9c9edd7c
pliniopereira10 <plinio.pereira10@gmail.com>
2022-11-20 18:51:24 -0300

    If applied, this will be commit initial will create client-control-api project

---
+++ backend/src/main/java/edu/pliniopereira10/clientcontrol/ClientControlApplication.java
@@ -0,0 +1,13 @@
+package edu.pliniopereira10.clientcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientControlApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/edu/pliniopereira10/clientcontrol/ClientControlApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pliniopereira10.clientcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
