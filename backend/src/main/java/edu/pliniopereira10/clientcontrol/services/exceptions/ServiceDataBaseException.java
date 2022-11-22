3d371a5924f6199bddfc7dc4c958cc0ecac5c209
pliniopereira10 <plinio.pereira10@gmail.com>
2022-11-22 18:43:35 -0300

    If applied, this commit will create the ServiceDataBaseException class

---
+++ backend/src/main/java/edu/pliniopereira10/clientcontrol/services/exceptions/ServiceDataBaseException.java
@@ -0,0 +1,10 @@
+package edu.pliniopereira10.clientcontrol.services.exceptions;
+
+public class ServiceDataBaseException extends RuntimeException {
+	private static final long serialVersionUID = 1L;
+	
+	public ServiceDataBaseException(String msg) {
+		super(msg);
+	}
+	
+}
