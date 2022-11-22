e16e7777a84f890052afb7146ecf8d3eff1c4a24
pliniopereira10 <plinio.pereira10@gmail.com>
2022-11-22 18:44:34 -0300

    If applied, this commit will update the ResourceExceptionHandler class

--- backend/src/main/java/edu/pliniopereira10/clientcontrol/controllers/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/edu/pliniopereira10/clientcontrol/controllers/exceptions/ResourceExceptionHandler.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import edu.pliniopereira10.clientcontrol.services.exceptions.ServiceDataBaseException;
 import edu.pliniopereira10.clientcontrol.services.exceptions.ServiceNotFoundExpetion;
 
 @ControllerAdvice
@@ -17,14 +18,29 @@ public class ResourceExceptionHandler {
 	@ExceptionHandler(ServiceNotFoundExpetion.class)
 	public ResponseEntity<StandardError> entityNotFound(ServiceNotFoundExpetion e,
 			HttpServletRequest request) {
+		HttpStatus status = HttpStatus.NOT_FOUND;
 		StandardError error = new StandardError();
 		error.setTimestamp(Instant.now());
-		error.setStatus(HttpStatus.NOT_FOUND.value());
+		error.setStatus(status.value());
 		error.setError("Resource not found");
 		error.setMessage(e.getMessage());
 		error.setPath(request.getRequestURI());
 
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+		return ResponseEntity.status(status).body(error);
+	}
+
+	@ExceptionHandler(ServiceDataBaseException.class)
+	public ResponseEntity<StandardError> entityNotFound(ServiceDataBaseException e,
+			HttpServletRequest request) {
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError error = new StandardError();
+		error.setTimestamp(Instant.now());
+		error.setStatus(status.value());
+		error.setError("Resource not found");
+		error.setMessage(e.getMessage());
+		error.setPath(request.getRequestURI());
+
+		return ResponseEntity.status(status).body(error);
 	}
 
 }
