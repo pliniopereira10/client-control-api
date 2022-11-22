134b01f6d03bd23864178ec28ce249033c2a4f48
pliniopereira10 <plinio.pereira10@gmail.com>
2022-11-22 12:07:41 -0300

    If applied, this commit will create the getAllClients method
    in endpoint GET/clients

--- backend/src/main/java/edu/pliniopereira10/clientcontrol/controllers/ClientController.java
+++ backend/src/main/java/edu/pliniopereira10/clientcontrol/controllers/ClientController.java
@@ -1,10 +1,28 @@
 package edu.pliniopereira10.clientcontrol.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import edu.pliniopereira10.clientcontrol.dtos.ClientDto;
+import edu.pliniopereira10.clientcontrol.services.ClientService;
+
 @RestController
-@RequestMapping(value =  "/clients")
+@RequestMapping(value = "/clients")
 public class ClientController {
+
+	@Autowired
+	ClientService clientService;
+
+	@GetMapping
+	public ResponseEntity<List<ClientDto>> getAllClients() {
+		List<ClientDto> clientDtos = clientService.findAll();
+
+		return ResponseEntity.ok().body(clientDtos);
+	}
 
 }
