94fe4d66057365658f7021181a0fb6642db136d6
pliniopereira10 <plinio.pereira10@gmail.com>
2022-11-22 17:04:07 -0300

    If applied, this commit will create the PUT/clients/{id} endpoint

--- backend/src/main/java/edu/pliniopereira10/clientcontrol/controllers/ClientController.java
+++ backend/src/main/java/edu/pliniopereira10/clientcontrol/controllers/ClientController.java
@@ -8,6 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -46,6 +47,11 @@ public class ClientController {
 		return ResponseEntity.created(uri).body(clientDto);
 	}
 	
-	
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<ClientDto> updateClient(@PathVariable Long id, @RequestBody ClientDto clientDto) {
+		clientDto = clientService.update(id, clientDto);
+		
+		return ResponseEntity.ok().body(clientDto);
+	}
 
 }
