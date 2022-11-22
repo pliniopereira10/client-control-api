7ade16a79dee72b482be2a14ef65812ee8b5919a
pliniopereira10 <plinio.pereira10@gmail.com>
2022-11-22 12:03:36 -0300

    If applied, this commit will create the findAll method

--- backend/src/main/java/edu/pliniopereira10/clientcontrol/services/ClientService.java
+++ backend/src/main/java/edu/pliniopereira10/clientcontrol/services/ClientService.java
@@ -1,8 +1,27 @@
 package edu.pliniopereira10.clientcontrol.services;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import edu.pliniopereira10.clientcontrol.dtos.ClientDto;
+import edu.pliniopereira10.clientcontrol.models.ClientModel;
+import edu.pliniopereira10.clientcontrol.repositories.ClientRepository;
 
 @Service
 public class ClientService {
+
+	@Autowired
+	ClientRepository clientRepository;
+
+	@Transactional(readOnly = true)
+	public List<ClientDto> findAll() {
+		List<ClientModel> clientModels = clientRepository.findAll();
+
+		return clientModels.stream().map(x -> new ClientDto(x)).collect(Collectors.toList());
+	}
 
 }
