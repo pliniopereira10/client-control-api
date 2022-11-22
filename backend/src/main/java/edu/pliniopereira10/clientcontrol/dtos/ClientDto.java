f4230a221ca1590406a6d471170e601d68e4ca90
pliniopereira10 <plinio.pereira10@gmail.com>
2022-11-22 09:39:41 -0300

    If applied, this commit will create the ClientDto class

---
+++ backend/src/main/java/edu/pliniopereira10/clientcontrol/dtos/ClientDto.java
@@ -0,0 +1,87 @@
+package edu.pliniopereira10.clientcontrol.dtos;
+
+import java.time.Instant;
+
+import edu.pliniopereira10.clientcontrol.models.ClientModel;
+
+public class ClientDto {
+	
+	private Long id;
+	private String name;
+	private String cpf;
+	private Double income;
+	private Instant birthDate;
+	private Integer children;
+	
+	public ClientDto() {
+
+	}
+
+	public ClientDto(Long id, String name, String cpf, Double income, Instant birthDate, Integer children) {
+		this.id = id;
+		this.name = name;
+		this.cpf = cpf;
+		this.income = income;
+		this.birthDate = birthDate;
+		this.children = children;
+	}
+	
+	public ClientDto(ClientModel model) {
+		this.id = model.getId();
+		this.name = model.getName();
+		this.cpf = model.getCpf();
+		this.income = model.getIncome();
+		this.birthDate = model.getBirthDate();
+		this.children = model.getChildren();
+	}
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Double getIncome() {
+		return income;
+	}
+
+	public void setIncome(Double income) {
+		this.income = income;
+	}
+
+	public Instant getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(Instant birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public Integer getChildren() {
+		return children;
+	}
+
+	public void setChildren(Integer children) {
+		this.children = children;
+	}
+	
+	
+}
