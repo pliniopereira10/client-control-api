c8fb6e3f94e90d8d80e8ad1c6f5ce10e3b8162c0
pliniopereira10 <plinio.pereira10@gmail.com>
2022-11-22 16:42:25 -0300

    If applie, this commit will create POST/clients endpoint

--- backend/src/main/java/edu/pliniopereira10/clientcontrol/controllers/ClientController.java
+++ backend/src/main/java/edu/pliniopereira10/clientcontrol/controllers/ClientController.java
@@ -1,13 +1,17 @@
 package edu.pliniopereira10.clientcontrol.controllers;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import edu.pliniopereira10.clientcontrol.dtos.ClientDto;
 import edu.pliniopereira10.clientcontrol.services.ClientService;
@@ -25,12 +29,23 @@ public class ClientController {
 
 		return ResponseEntity.ok().body(clientDtos);
 	}
-	
+
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<ClientDto> getOneClient(@PathVariable Long id){
+	public ResponseEntity<ClientDto> getOneClient(@PathVariable Long id) {
 		ClientDto clientDto = clientService.finById(id);
-		
+
 		return ResponseEntity.ok().body(clientDto);
 	}
+
+	@PostMapping
+	public ResponseEntity<ClientDto> saveClient(@RequestBody ClientDto clientDto) {
+		clientDto = clientService.insert(clientDto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+				.buildAndExpand(clientDto.getId()).toUri();
+
+		return ResponseEntity.created(uri).body(clientDto);
+	}
+	
+	
 
 }
